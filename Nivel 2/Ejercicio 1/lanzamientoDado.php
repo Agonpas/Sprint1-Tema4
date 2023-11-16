9617407fcf1636a68fbdb847c1c2a8530d89436d
Agonpas <agonpas@gmail.com>
2023-11-16 11:18:26 +0100

    Puedo lanzar 5 dados

--- Nivel 2/Ejercicio 1/lanzamientoDado.php
+++ Nivel 2/Ejercicio 1/lanzamientoDado.php
@@ -1,21 +1,50 @@
 <?php
-$dado1 = ["As", "K", "Q", "J", "8", "7"];
-echo "¿Quieres lanzar los dados? 1-Si. 2-No";
-$eleccion = fgets(STDIN);
-switch ($eleccion) {
-    case 1:
-        lanzarDado($dado1);
-        break;
+$solo1Dado = ["As", "K", "Q", "J", "8", "7"];
+$cincoDados = [
+    "Dado 1"=> ["As", "K", "Q", "J", "8", "7"],
+    "Dado 2"=> ["As", "K", "Q", "J", "8", "7"],
+    "Dado 3"=> ["As", "K", "Q", "J", "8", "7"],
+    "Dado 4"=> ["As", "K", "Q", "J", "8", "7"],
+    "Dado 5"=> ["As", "K", "Q", "J", "8", "7"]
+];
+$tiradas = 0;
 
-    case 2:
-        echo "No lanzamos dados";
-        break;
 
-    default:
-        echo "Opcion no valida";
+do {
+    echo " 0-No lanzar mas dados \n 1-Lanzar un dado. \n 2-Lanzar cinco dados \n";
+    $eleccion = fgets(STDIN);
+    switch ($eleccion) {
+        case 0:
+            echo "Gracias por jugar" . PHP_EOL;
+            break;
+
+        case 1:
+            lanzarDado($solo1Dado);
+            $tiradas +=1;
+            break;
+    
+        case 2:
+            lanzarCincoDados($cincoDados);
+            $tiradas +=1;
+            break;
+    
+        default:
+            echo "Opcion no valida";
+    }
+
+} while ($eleccion != 0);
+echo "Has tirado dados un total de " . $tiradas . " vecces";
+
+
+function lanzarDado($solo1Dado) {
+    $resultadoDado1 = $solo1Dado[array_rand($solo1Dado)];
+    echo "El resultado del dado 1 es " . $resultadoDado1 . PHP_EOL;
 }
-function lanzarDado($dado1) {
-    $resultadoDado1 = $dado1[array_rand($dado1)];
-    echo "El resultado del dado 1 es " . $resultadoDado1;
+function lanzarCincoDados($cincoDados) {
+    foreach ($cincoDados as $dado => $cara) {
+        $resultado = $cara[array_rand($cara)];
+        echo "El resutltado del dado " . $dado . " es: "  . $resultado . PHP_EOL;
+    }
+
 }
 ?>
